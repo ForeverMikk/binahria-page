67ec097faa10d124aab0bc16fb5883d9f98ac663
unknown <michaelmolina679@gmail.com>
2022-09-26 19:26:50 -0500

    new changes in contact page

--- src/views/Contact/ContactHeader/ContactHeader.jsx
+++ src/views/Contact/ContactHeader/ContactHeader.jsx
@@ -12,8 +12,32 @@ const ContactHeader = () => {
     return (
         <div className='contact-header' id='contact'>  
             <Navbar logo={logo}/>
+            
+            <div className="top">
+                <span>/ {t('conocenos.title')}</span>
+            </div>
+
             <div id="main-text" className='title'>
                 <p>{t('contact.title')}</p>
+                <p className='subtitle'>{t('contact.subtitle')}</p>
+            </div>
+
+            <div className="bottom-text">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus molestias nam laboriosam porro quis fugiat tempore dicta rerum quos est eligendi, consequatur, itaque odio soluta ratione harum minus dolor omnis.</p>
+
+                <div className="buttons">
+
+                    <div className="contact-button">
+                        <p>Ver formulario de contacto</p>
+                        <span>+ Info</span>
+                    </div>
+
+                    <div className="meet-button">
+                        <p>Agendar una reunión</p>
+                        <span>+ Info</span>
+                    </div>
+                    
+                </div>
             </div>
         </div>
     )
