dbd1543d1799865d0de448c50a1240d908590084
unknown <michaelmolina679@gmail.com>
2022-09-27 15:18:22 -0500

    add video in contact section

--- src/views/Contact/ContactBottom/ContactBottom.jsx
+++ src/views/Contact/ContactBottom/ContactBottom.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import './ContactBottom.css';
+import cubos from './../../../assets/videos/cubos.mp4';
 import ContactField from './ContactField/ContactField';
 
 const ContactBottom = () => {
+    
+    useEffect(() => {
+        // let framenumber = 0;
+        let playbackConst = 500;
+        // let videoHeight = document.getElementById("video");
+        let vid = document.getElementById("v0");
+
+        // vid.addEventListener('loadedmetadata', () => {
+            // videoHeight.style.height = Math.floor(vid.duration) * playbackConst + "px";
+            // console.log(videoHeight.style.height);
+        // });
+
+        function scrollPlay() {
+            let frameNumber = (window.pageYOffset)/playbackConst;
+            vid.currentTime = frameNumber;
+            window.requestAnimationFrame(scrollPlay);
+        }
+
+        window.requestAnimationFrame(scrollPlay);
+
+    }, [])
+    
+    
     return (
         <section className="contact-bottom">
             
@@ -12,8 +36,12 @@ const ContactBottom = () => {
                     <h3>Déjanos llevarte más lejos de lo que nunca has estado.</h3>
                     <button>PROGRAMA UNA REUNIÓN &rarr;</button>
                 </div>
-                <div className="video">
 
+                <div className="video" id='video'>
+                    <div className="set-height" id='set-height'></div>
+                    <video id='v0' tabIndex="0" autoBuffer preload >
+                        <source type='video/mp4' src={cubos} />
+                    </video>
                 </div>
             </div>
 
@@ -26,14 +54,13 @@ const ContactBottom = () => {
                 </div>
 
                 <div className="social second">
-
-                    <div className="social uno">
+                    <div className="container uno">
                         <h3 className="title">LLAMADAS</h3>
                         <ContactField title='Teléfono' description='(+52) 55 2568 0859' />
                         <ContactField title='Videollamadas' description='calendly/binahria' />
                     </div>
 
-                    <div className="social dos">
+                    <div className="container dos">
                         <h3 className="title">HORARIO</h3>
                         <ContactField title='Lunes a viernes' description='8:00 - 10:00 hrs' />
                         <ContactField title='Sábados' description='8:00 - 12:00 hrs' />
@@ -44,6 +71,5 @@ const ContactBottom = () => {
         </section>
     )
 }
-
 
 export default ContactBottom;
--- src/views/Contact/ContactBottom/ContactField/ContactField.jsx
+++ src/views/Contact/ContactBottom/ContactField/ContactField.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './ContactField.css';
+
 const ContactField = ({title, description}) => {
     return <div className='contact-field'>
         <h4 className='title'>{title}</h4>
-        <p className='description'>{description}</p>           
+        <p className='description'>{description}</p>
     </div>
 }
 
