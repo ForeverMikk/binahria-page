6c1487805ef93d71a8ddcabaee5cb1d9ae217421
unknown <michaelmolina679@gmail.com>
2022-10-07 12:49:36 -0500

    add new video background

--- src/views/ProductView/ProductHeader/ProductHeader.jsx
+++ src/views/ProductView/ProductHeader/ProductHeader.jsx
@@ -3,20 +3,22 @@ import React from "react"
 import './ProductHeader.css';
 import Navbar from "../../../components/Navbar/Navbar"
 import logo from '../../../assets/images/logo/logo-white.png';
+import background from '../../../assets/videos/product-background.mp4';
 
 const ProductHeader = () => {
     return (
         <div className='product-header'>  
             <Navbar logo={logo}/>
-            <div className="video-container">
-                <iframe
+            <video className="video-container" autoPlay muted loop>
+                <source src={background} type="video/mp4" />
+                {/* <iframe
                     src="https://www.youtube.com/embed/mkhvraSWqS0?controls=0&autoplay=1&mute=1&playsinline=1&playlist=mkhvraSWqS0&loop=1&showinfo=0" 
                     title=" " 
                     frameborder="0" 
                     allow="autoplay;" 
                     >
-                </iframe>
-            </div>
+                </iframe> */}
+            </video>
         </div>
     )
 }
