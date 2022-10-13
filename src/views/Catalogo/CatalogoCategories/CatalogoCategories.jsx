a8f531ee4d71a9baea624154b155cc2b857767b1
unknown <michaelmolina679@gmail.com>
2022-10-13 14:45:19 -0500

    deleting unused code

--- src/views/Catalogo/CatalogoCategories/CatalogoCategories.jsx
+++ src/views/Catalogo/CatalogoCategories/CatalogoCategories.jsx
@@ -10,13 +10,6 @@ import { corporativos, gubernamentales, politicos } from '../../../variables/pro
 import CategoryCarousel from './CategoryCarousel/CategoryCarousel';
 import CategoryDropdown from './CategoryDropdown/CategoryDropdown';
 
-const options = [
-    { value: 'category-1', label: 'Categoria 1', description: "este texto es de prueba", img: img1},
-    { value: 'category-2', label: 'Categoria 2', description: "este texto es de prueba", img: img1},
-    { value: 'category-3', label: 'Categoria 3', description: "este texto es de prueba", img: img1},
-    { value: 'category-4', label: 'Categoria 4', description: "este texto es de prueba", img: img1},
-    { value: 'category-5', label: 'Categoria 5', description: "este texto es de prueba", img: img1},
-]
 
 const CatalogoCategories = () => {
     return (
@@ -33,7 +26,6 @@ const CatalogoCategories = () => {
             <CategoryCarousel 
                 id={1} 
                 title="Político Electoral" 
-                categories={options} 
                 background='#000'
                 products={politicos}
                 productType='politic'
@@ -42,7 +34,6 @@ const CatalogoCategories = () => {
             <CategoryCarousel 
                 id={2} 
                 title="Político Gubernamental" 
-                categories={options} 
                 background='#9147FB' 
                 products={gubernamentales}
                 productType='goberment'
@@ -51,7 +42,6 @@ const CatalogoCategories = () => {
             <CategoryCarousel 
                 id={3} 
                 title="Político Corporativo" 
-                categories={options} 
                 background='#E8E8E8' 
                 products={corporativos}
                 productType='corporative'
