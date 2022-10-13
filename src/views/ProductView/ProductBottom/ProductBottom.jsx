3d8f31650893bfcca38975381c3e37a4764c9be0
unknown <michaelmolina679@gmail.com>
2022-10-13 18:36:17 -0500

    add new styles in product bottom

--- src/views/ProductView/ProductBottom/ProductBottom.jsx
+++ src/views/ProductView/ProductBottom/ProductBottom.jsx
@@ -7,18 +7,26 @@ const ProductBottom = () => {
     return (
         <div className='product-bottom'>
 
-            <img className='graphics-img' src={graphics} alt="" />
+            <h3>PROYECTO PREVIO: <span>TABLERO SAEKO</span> </h3>
 
-            <div className='text'>
-                <h3>PROYECTOS</h3>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
-                    sed diam nonummy nibh euismod tincidunt ut laoreet
-                    dolore magna aliquam erat 
-                </p>
-                <button>PROGRAMA UNA REUNIÓN &rarr;</button>
+            <div className="content">
+                <img className='graphics-img' src={graphics} alt="" />
 
+                <div className='text'>
+                    <p>
+                    Tablero Saeko es un producto 
+                    que desarrollamos para que 
+                    nuestros clientes tengan mejor 
+                    percepción sobre los cambios 
+                    financieros que sufre su empresa. Desde entonces ha evolucionado hasta convertirse en su 
+                    herramienta “todo en uno” que 
+                    muestra todo lo que las personas necesitan saber para administrar no solo su área contable, 
+                    sino también su negocio.
+                    </p>
+                    <button>PROGRAMA UNA REUNIÓN &rarr;</button>
+                </div>
             </div>
+
         </div>
     )
 }
