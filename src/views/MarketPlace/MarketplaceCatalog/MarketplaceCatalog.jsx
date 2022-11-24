8287d9fbbd935e2034f78b8debfde1b4566fdf95
unknown <michaelmolina679@gmail.com>
2022-11-23 18:24:45 -0600

    add new funtionalities in marketplace catalog

--- src/views/MarketPlace/MarketplaceCatalog/MarketplaceCatalog.jsx
+++ src/views/MarketPlace/MarketplaceCatalog/MarketplaceCatalog.jsx
@@ -3,58 +3,111 @@ import React from 'react';
 import './MarketplaceCatalog.css';
 import img from '../../../assets/images/blank_image.png'
 import MarketplaceProduct from './MarketplaceProduct/MarketplaceProduct';
-import MarketTabs from './MarketTabs';
-
-
+import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 
 const MarketplaceCatalog = () => {
     return (
         <div className="marketplace-catalog">
 
-            <MarketTabs />
             <h2>/ TOP EN INVESTIGACIÓN</h2>
 
-            <div className="selectors">
-                <div className='underline' >
-                    <h3 className="politic">Política</h3>
-                </div>
-                
-                <div className='underline' >
-                    <h3 className="economy">Economía</h3>
-                </div>
+            <Tabs>
 
-                <div className='underline' >
-                    <h3 className="fintech">Fintech</h3>
-                </div>
-            </div>
+                <TabList className='selectors'>
+                    <Tab className='underline'>
+                        <h3 className="politic">Política</h3>
+                    </Tab>
+                    
+                    <Tab className='underline' >
+                        <h3 className="economy">Economía</h3>
+                    </Tab>
+
+                    <Tab className='underline' >
+                        <h3 className="fintech">Fintech</h3>
+                    </Tab>                   
+                </TabList>
                 
-                
-            <div className="products">
-                <MarketplaceProduct
-                    title='Investigación y Ciencia' 
-                    description='Ofrece información más importante sobre la ciencia y la técnica en todo  el mundo'
-                    date='SEPTIEMBRE 2022'
-                    img={img}
-                    />
-                <MarketplaceProduct 
-                    title='Investigación y Ciencia' 
-                    description='Ofrece información más importante sobre la ciencia y la técnica en todo  el mundo'
-                    date='SEPTIEMBRE 2022'
-                    img={img}
-                    />
-                <MarketplaceProduct 
-                    title='Investigación y Ciencia' 
-                    description='Ofrece información más importante sobre la ciencia y la técnica en todo  el mundo'
-                    date='SEPTIEMBRE 2022'
-                    img={img}
-                    />
-                <MarketplaceProduct 
-                    title='Investigación y Ciencia' 
-                    description='Ofrece información más importante sobre la ciencia y la técnica en todo  el mundo'
-                    date='SEPTIEMBRE 2022'
-                    img={img}
-                    />
-            </div>
+                <TabPanel className="products" style={{background: '#E8E8E8'}}>
+                    <MarketplaceProduct
+                        title='Investigación y Ciencia' 
+                        description='Ofrece información más importante sobre la ciencia y la técnica en todo  el mundo'
+                        date='SEPTIEMBRE 2022'
+                        img={img}
+                        />
+                    <MarketplaceProduct 
+                        title='Investigación y Ciencia' 
+                        description='Ofrece información más importante sobre la ciencia y la técnica en todo  el mundo'
+                        date='SEPTIEMBRE 2022'
+                        img={img}
+                        />
+                    <MarketplaceProduct 
+                        title='Investigación y Ciencia' 
+                        description='Ofrece información más importante sobre la ciencia y la técnica en todo  el mundo'
+                        date='SEPTIEMBRE 2022'
+                        img={img}
+                        />
+                    <MarketplaceProduct 
+                        title='Investigación y Ciencia' 
+                        description='Ofrece información más importante sobre la ciencia y la técnica en todo  el mundo'
+                        date='SEPTIEMBRE 2022'
+                        img={img}
+                        />
+                </TabPanel>
+
+                <TabPanel className="products" style={{background: '#1E1E1E'}}>
+                    <MarketplaceProduct
+                        title='Investigación y Ciencia' 
+                        description='Ofrece información más importante sobre la ciencia y la técnica en todo  el mundo'
+                        date='SEPTIEMBRE 2022'
+                        img={img}
+                        />
+                    <MarketplaceProduct 
+                        title='Investigación y Ciencia' 
+                        description='Ofrece información más importante sobre la ciencia y la técnica en todo  el mundo'
+                        date='SEPTIEMBRE 2022'
+                        img={img}
+                        />
+                    <MarketplaceProduct 
+                        title='Investigación y Ciencia' 
+                        description='Ofrece información más importante sobre la ciencia y la técnica en todo  el mundo'
+                        date='SEPTIEMBRE 2022'
+                        img={img}
+                        />
+                    <MarketplaceProduct 
+                        title='Investigación y Ciencia' 
+                        description='Ofrece información más importante sobre la ciencia y la técnica en todo  el mundo'
+                        date='SEPTIEMBRE 2022'
+                        img={img}
+                        />
+                </TabPanel>
+
+                <TabPanel className="products">
+                    <MarketplaceProduct
+                        title='Investigación y Ciencia' 
+                        description='Ofrece información más importante sobre la ciencia y la técnica en todo  el mundo'
+                        date='SEPTIEMBRE 2022'
+                        img={img}
+                        />
+                    <MarketplaceProduct 
+                        title='Investigación y Ciencia' 
+                        description='Ofrece información más importante sobre la ciencia y la técnica en todo  el mundo'
+                        date='SEPTIEMBRE 2022'
+                        img={img}
+                        />
+                    <MarketplaceProduct 
+                        title='Investigación y Ciencia' 
+                        description='Ofrece información más importante sobre la ciencia y la técnica en todo  el mundo'
+                        date='SEPTIEMBRE 2022'
+                        img={img}
+                        />
+                    <MarketplaceProduct 
+                        title='Investigación y Ciencia' 
+                        description='Ofrece información más importante sobre la ciencia y la técnica en todo  el mundo'
+                        date='SEPTIEMBRE 2022'
+                        img={img}
+                        />
+                </TabPanel>
+            </Tabs>
                 
         </div>
     )
